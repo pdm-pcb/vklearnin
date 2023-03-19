4c7b92837fa57672a68906e22d9138949af4977a
patrick <1423602+pdm-pcb@users.noreply.github.com>
2023-03-18 19:23:15 -0500

    Slightly better SDL dep checking/etc

--- 18Lights/vklearnin/include/vklearnin/system/pch.hpp
+++ 18Lights/vklearnin/include/vklearnin/system/pch.hpp
@@ -5,25 +5,19 @@
 // https://github.com/KhronosGroup/Vulkan-Hpp#designated-initializers
 #define VULKAN_HPP_NO_CONSTRUCTORS
 
-#define VULKAN_HPP_NO_EXCEPTIONS    // I perfer to check the return values
+#define VULKAN_HPP_NO_EXCEPTIONS // I perfer to check the return values
+
 #define VULKAN_HPP_ASSERT_ON_RESULT // Assert on result can trip things up when
                                     // exceptions are disabled
 
 // Prevent needing to load every function by hand
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 
-// Platform specific WSI defines
-#if defined(VKL_LINUX)
-    #define VK_USE_PLATFORM_XCB_KHR
-#elif defined(VKL_WINDOWS)
-    #define VK_USE_PLATFORM_WIN32_KHR
-#endif
+#include <vulkan/vulkan.hpp>
 
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
-
-#include <vulkan/vulkan.hpp>
 
 #if defined(VKL_LINUX)
     #include <X11/Xlib.h>
